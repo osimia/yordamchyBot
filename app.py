93a3835650947eb48c36c0369c4c23c686f0d0b1
Akramkhon Osimi <6721067659@my.lerna.tj>
2025-06-27 11:30:37 +0500

    import24

--- app.py
+++ app.py
@@ -99,4 +99,9 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
